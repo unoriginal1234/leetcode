9096ec12259b9672375b032e13e77fdf28f8ec35
unoriginal1234 <eric.t.roth@gmail.com>
2023-08-09 15:53:10 -0700

    did it but had trouble converting to string in enough time

---
+++ LargestTimeFrameFromDigits.py
@@ -0,0 +1,37 @@
+def largestTimeFromDigits(arr):
+    
+    Time = []
+
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            for k in range(len(arr)):
+                for l in range(len(arr)):
+                    x = arr[i]*1000 + arr[j]*100 + arr[k]*10 + arr[l]
+                    Time.append(x)
+    
+    newTime = []
+    for i in range(len(Time)):
+        if Time[i] < 2400:
+            newTime.append(Time[i])
+    
+    if not newTime:
+        print("")
+    
+    newNewTime = []
+    for i in range(len(newTime)):
+        if newTime[i] % 100 < 59:
+            newNewTime.append(newTime[i])
+    
+    if not newNewTime:
+        print("")
+    
+    ans = max(newNewTime)
+
+    Hour = int(ans/100) 
+    Minute = ans - Hour * 100 
+
+    print(f"{Hour}:{Minute}")
+        
+
+arr = [1,2,5,9]
+largestTimeFromDigits(arr)
