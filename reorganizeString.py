e01c5364d31230642ad1e6cfe6065d87ba6d032b
unoriginal1234 <eric.t.roth@gmail.com>
2023-08-23 14:14:58 -0700

    solved

---
+++ reorganizeString.py
@@ -0,0 +1,40 @@
+
+def reorganizeString(s):
+    n = len(s)
+    Keys = []
+    Values = []
+    for i in range(n):
+        if s[i] not in Keys:
+            Keys.append(s[i])
+            count = 1
+            j = i + 1
+            while j <= n - 1:
+                if s[i] == s[j]:
+                    count += 1
+                j += 1
+            Values.append(count)
+
+    ans = ""
+    my_dict = dict(zip(Keys,Values))
+
+    last_let = {}
+
+    while my_dict:
+        next_let = max(my_dict, key=my_dict.get)
+        ans = ans + next_let
+        if last_let:
+            my_dict.update(last_let)
+        my_dict[next_let] = my_dict[next_let] - 1
+        if my_dict[next_let] == 0:
+            del my_dict[next_let]
+        else:
+            last_let = {next_let: my_dict.pop(next_let)}
+
+    if len(ans) == n:
+        return ans
+    else:
+        return ""
+
+s = "bfrbs"
+x = reorganizeString(s)
+print(x)
