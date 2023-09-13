c91227f49033fff7956adafdae17a3fc272ab923
unoriginal1234 <eric.t.roth@gmail.com>
2023-09-13 08:33:54 -0700

    solved, but slow

--- minDeletions.py
+++ minDeletions.py
@@ -17,15 +17,21 @@ def minDeletions(s):
         m = len(dict_valuesList)
         ans = 0
         for i in range(m - 1):
-            if dict_valuesList[i] == dict_valuesList[i + 1]:
-                dict_valuesList[i + 1] -= 1
+            j = i + 1
+            while dict_valuesList[i]> 0 and dict_valuesList[i] == dict_valuesList[j]:
+                # Can I do a seperate loop here to clear out the long string of similars?
+                dict_valuesList[j] -= 1
                 ans += 1
+                if j == m - 1:
+                    break
+                else:
+                    j += 1
 
         return ans
 
 
                 
 
-s = "aab"
+s = "bbcebab"
 x = minDeletions(s)
 print(x)
