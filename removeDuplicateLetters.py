02f3d200847e43853c437c2f8c65eef8e1dcd17c
unoriginal1234 <eric.t.roth@gmail.com>
2023-09-26 08:26:51 -0700

    couldnt solve

---
+++ removeDuplicateLetters.py
@@ -0,0 +1,41 @@
+
+
+def removeDuplicateLetters(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+
+    letters = []
+    for i in s:
+        x = s.count(i)
+        if x > 1:
+            letters.append(i)
+
+    letters.sort(reverse=True)
+    s1 =  list(s)
+    
+    for i in letters:
+        letters.remove(i)
+        s1.remove(i)
+
+    ans = ""
+    for i in s1:
+        ans = ans + i
+    
+    return(ans)
+
+
+# s = "bcabc"
+# if removeDuplicateLetters(s) == "abc":
+#     print('true')
+# else:
+#     print('false')
+
+s = "cbacdcbc"
+if removeDuplicateLetters(s) == "acdb":
+    print('true')
+else:
+    print('false')
+    print(removeDuplicateLetters(s))
+
